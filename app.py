b4de977c4203a2aa8c1e6ce392c52fd38ddd5642
Aruuu1 <m.aldiokgo@gmail.com>
2024-11-27 23:05:51 +0700

    revisi

--- app.py
+++ app.py
@@ -1,23 +1,65 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, url_for
+
 app = Flask(__name__)
 
+app.secret_key = 'secret'
+
 @app.route("/", methods=['GET', 'POST'])
-def login():
+def index():
     if request.method == "POST":
-        nama = request.form.get('nama')
-        nim = request.form.get('nim')
-        email = request.form.get('email')
-        jurusan = request.form.get('jurusan')
-        kode_matakuliah = request.form.get('kode_matakuliah')
-        matakuliah = request.form.get('matakuliah')
-        nilai = float(request.form.get("nilai", 0.0))
-        sks = int(request.form.get('sks', 0))
-        ipk = (nilai * sks) / sks
+        session['nama'] = request.form.get('nama')
+        session['nim'] = request.form.get('nim')
+        session['email'] = request.form.get('email')
+        session['jurusan'] = request.form.get('jurusan')
+        session['kode_matakuliah'] = request.form.get('kode_matakuliah')
+        session['matakuliah'] = request.form.get('matakuliah')
+        session['nilai'] = float(request.form.get("nilai", 0.0))
+        session['sks'] = int(request.form.get('sks', 0))
+        session['ipk'] = (session['nilai'] * session['sks']) / session['sks']
 
-        return render_template ("index.html", nama=nama, nim=nim, email=email, jurusan=jurusan, kode_matakuliah=kode_matakuliah, nilai=nilai, matakuliah=matakuliah, sks=sks, ipk=ipk)
+        return redirect(url_for('dashboard'))
 
-    return render_template('login.html')
+    return render_template('index.html')
 
+@app.route("/dashboard")
+def dashboard():
+    if 'nama' in session:
+        return render_template ('dashboard.html',
+        nama = session['nama'],
+        nim = session['nim'],
+        jurusan = session['jurusan'],
+        email = session['email'])
+
+    return redirect(url_for("index"))
+
+@app.route("/krs")
+def krs():
+    if 'nama' in session:
+        return render_template ('krs.html',
+        kode_matakuliah = session['kode_matakuliah'],
+        matakuliah = session['matakuliah'],
+        sks = session['sks'])
+
+    return redirect(url_for("index"))
+
+@app.route("/ipk")
+def ipk():
+    if 'nama' in session:
+        return render_template ('ipk.html',
+        ipk = session['ipk'],
+        sks = session['sks'])
+
+    return redirect(url_for("index"))
+
+@app.route("/daftarnilai")
+def daftarnilai ():
+    if 'nama' in session:
+        return render_template ('daftarnilai.html',
+        nilai = session['nilai'],
+        matakuliah = session['matakuliah'],
+        sks = session['sks'])
+
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
